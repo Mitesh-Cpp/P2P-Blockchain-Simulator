7486452ba2fef20a42cf5c49d2f098830464881b
Mitesh Khemani <miteshkhemani8@gmail.com>
2024-02-04 14:21:11 +0530

    added find_longest_chain() function

--- peer_utils.py
+++ peer_utils.py
@@ -1,1 +1,16 @@
-find_longest_chain(genesis_block)
+def find_longest_chain(root):
+    def dfs(node, current_path):
+        if not node:
+            return current_path
+
+        current_path.append(node)
+
+        longest_path = current_path
+        for child in node.children:
+            child_path = dfs(child, current_path.copy())
+            if len(child_path) > len(longest_path):
+                longest_path = child_path
+
+        return longest_path
+
+    return dfs(root, [])
