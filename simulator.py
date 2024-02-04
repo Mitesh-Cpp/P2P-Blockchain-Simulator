1554794f797688d046f5f93a5f6d4ab30ccb3e56
Mitesh Khemani <miteshkhemani8@gmail.com>
2024-02-04 14:22:18 +0530

    added mine_block events at time 0 and random transaction_events in event_queue

--- simulator.py
+++ simulator.py
@@ -4,6 +4,7 @@ import csv
 import heapq
 from peer import peer
 from connect_graph import connect_graph
+from handle_events_queue import handle_events_queue
 
 def generate_peer_set(total_peers, z0_percent, z1_percent):
     peers_set = [i for i in range(1, total_peers + 1)]
@@ -27,7 +28,7 @@ def main():
     args = parser.parse_args()
     peers_set, z0_peers_set, z1_peers_set = generate_peer_set(args.total_peers, args.z0_percent, args.z1_percent)
 
-    all_peers = []  # [[peer_id, z0, z1, initial_balance]]
+    all_peers = [] 
 
     h = 1/(args.total_peers*(10 - 9*args.z1_percent/100))    # low CPU hashing power
     for peer_id in peers_set:
@@ -36,30 +37,24 @@ def main():
         hashing_power = 10 * h if is_z1 else h
         all_peers.append(peer(peer_id, args.total_peers, is_z0, is_z1, args.initial_balance, hashing_power))
 
-    for peer_obj in all_peers:
-        peer_obj.display_properties()
-
     all_event_list = [] # This is a priority queue which keeps the events sorted based on time
 
     counter = 0
     for peer_obj in all_peers:
         heapq.heappush(all_event_list, peer_obj.generate_transaction_event(args.total_peers * args.Tx,
                                                                             args.total_peers, args.initial_balance, args.To))
-        # heapq.heappush(all_event_list, peer_obj.generate_block_event(args.total_peers * args.Tb, args.To))
-        print(counter)
-        counter = counter + 1
+        heapq.heappush(all_event_list, peer_obj.generate_mine_block_event(0))
     
-    flattened_list = [element for event in all_event_list for element in event]
-    # sorted_list = sorted(flattened_list, key=lambda x: x[1])
     csv_file_path = "output_events.csv"
     with open(csv_file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
         for element in heapq.merge(*all_event_list, key=lambda x: x[1]):
             writer.writerow(element)
 
-    print("All events written to 'output_events.csv' file..!!")
     connect_graph(all_peers)
-    
+    for peer_obj in all_peers:
+        peer_obj.display_properties()
+    handle_events_queue(all_event_list, all_peers)
 
 if __name__ == "__main__":
     main()
