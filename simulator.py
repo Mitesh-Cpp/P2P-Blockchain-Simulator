b8528f019e299bb65a27279c2e1e5b8f8cbaad4f
Mitesh Khemani <miteshkhemani8@gmail.com>
2024-02-05 22:24:46 +0530

    minor fix

--- simulator.py
+++ simulator.py
@@ -3,11 +3,13 @@ import random
 import csv
 import heapq
 from peer import peer
+from block import block
+import uuid
 from connect_graph import connect_graph
 from handle_events_queue import handle_events_queue
 
 def generate_peer_set(total_peers, z0_percent, z1_percent):
-    peers_set = [i for i in range(1, total_peers + 1)]
+    peers_set = [i for i in range(0, total_peers)]
     z0_peers_set = random.sample(peers_set, int(z0_percent * total_peers / 100))
     z1_peers_set = random.sample(peers_set, int(z1_percent * total_peers / 100))
     return peers_set, z0_peers_set, z1_peers_set
@@ -19,7 +21,7 @@ def main():
     parser.add_argument('--z0_percent', type=float, default=50.0, help='z0 percent (slow peers percent)')
     parser.add_argument('--z1_percent', type=float, default=50.0, help='z1 percent (low cpu peers percent)')
     # mean time for interarrival of transactions (Tx) in seconds
-    parser.add_argument('--Tx', type=int, default=400, help='Mean Time for Exponential distribution of interarrival transaction times') 
+    parser.add_argument('--Tx', type=int, default=1000, help='Mean Time for Exponential distribution of interarrival transaction times') 
     # mean time for interarrival of blocks (Tb) in seconds
     parser.add_argument('--Tb', type=int, default=600000, help='Mean Time for Exponential distribution of interarrival block times') 
     # observation period / simulator running period
@@ -31,11 +33,13 @@ def main():
     all_peers = [] 
 
     h = 1/(args.total_peers*(10 - 9*args.z1_percent/100))    # low CPU hashing power
+    genesis_block_root_id = str(uuid.uuid4()) # block(str(uuid.uuid4()), None, self.id, 0)
     for peer_id in peers_set:
         is_z0 = not (peer_id in z0_peers_set)  # interpret 0 as slow and 1 as fast
         is_z1 = not (peer_id in z1_peers_set)  # interpret 0 as low and 1 as high
         hashing_power = 10 * h if is_z1 else h
-        all_peers.append(peer(peer_id, args.total_peers, is_z0, is_z1, args.initial_balance, hashing_power, args.Tb))
+        genesis_block = block(genesis_block_root_id, None, None, 0)
+        all_peers.append(peer(peer_id, args.total_peers, is_z0, is_z1, args.initial_balance, hashing_power, args.Tb, genesis_block))
 
     all_event_list = [] # This is a priority queue which keeps the events sorted based on time
 
